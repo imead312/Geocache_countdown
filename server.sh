873dd27d299aab02d707eaf1bf4e532c8ba94753
imead312 <87513735+imead312@users.noreply.github.com>
2022-08-16 16:31:49 +1000

    Add time unit selector buttons

--- server.sh
+++ server.sh
@@ -11,6 +11,6 @@ then
 else
     echo "Starting new screen session..."
     cd /Users/isaacmead/Code/JavaScript/perthCountdown/public || echo "Directory not found, no server created."
-    screen -dmS perthAppServer python -m http.server || echo "Error creating HTTP server"
-    echo "Server started successfully on port 8000. Use 'screen -r server' to reconnect."
+    screen -dmS perthAppServer python -m http.server 8000 || echo "Error creating HTTP server"
+    echo "Server started successfully on port 8000. Use 'screen -r to reconnect (or screen -r perthAppServer)' to reconnect."
 fi
