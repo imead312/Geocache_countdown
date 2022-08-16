bf1f2c8c4cbadf636f24af73fc3a28341fdbe40b
imead312 <87513735+imead312@users.noreply.github.com>
2022-08-16 20:22:41 +1000

    Hide 0-values in countdown, add plural function

--- public/countdown.js
+++ public/countdown.js
@@ -6,12 +6,12 @@ countDownDate = new Date("October 17, 2022 09:00:00")
 countDownTime = countDownDate.getTime();
 
 timeUnits = {
-    months: {active: true, factor: null, value: 0, id: "mon", name: " months"},
-    weeks: {active: true, factor: (1000 * 60 * 60 * 24 * 7), value: 0, id: "wks", name: " weeks"},
-    days: {active: true, factor: (1000 * 60 * 60 * 24), value: 0, id: "day", name: " days"},
-    hours: {active: true, factor: (1000 * 60 * 60), value: 0, id: "hrs", name: " hours"},
-    minutes: {active: true, factor: (1000 * 60), value: 0, id: "min", name:  " minutes"},
-    seconds: {active: true, factor: (1000), value: 0, id: "sec", name: " seconds"}
+    months: {active: true, factor: null, value: 0, id: "mon", name: " month"},
+    weeks: {active: true, factor: (1000 * 60 * 60 * 24 * 7), value: 0, id: "wks", name: " week"},
+    days: {active: true, factor: (1000 * 60 * 60 * 24), value: 0, id: "day", name: " day"},
+    hours: {active: true, factor: (1000 * 60 * 60), value: 0, id: "hrs", name: " hour"},
+    minutes: {active: true, factor: (1000 * 60), value: 0, id: "min", name:  " minute"},
+    seconds: {active: true, factor: (1000), value: 0, id: "sec", name: " second"}
 }
 
 function updateTime () {
@@ -19,48 +19,40 @@ function updateTime () {
     var currentDate = new Date();
     var now = currentDate.getTime();
     var distance = countDownTime - now;
-
-    console.log(now, distance);
     
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("debug").innerHTML = "We made it!"
     } else {
-        Object.keys(timeUnits).forEach((key, index) => {
-
+        Object.keys(timeUnits).forEach((key, _) => {
             let unit = timeUnits[key];
-
-        
             if (unit.active) {
-
-                
                 if (unit.id == "mon") {
                     unit.value = (12 + countDownDate.getMonth() - currentDate.getMonth()) % 12;
-                    // timeUnits.months.value = monthVal;
                     distance -= getDaysInMonths(currentDate, unit.value);
-
                 } else {
                     unit.value = Math.floor(distance / unit.factor)
                     distance -= unit.value * unit.factor;
                 }
-                document.getElementById(unit.id).innerHTML = unit.value + unit.name            
+                
+                if (unit.value > 0 || unit.id == "sec") {
+                    document.getElementById(unit.id).innerHTML = unit.value + unit.name + (unit.value != 1 ? "s" : "");    
+                } else {
+                    console.log(unit.name + " value currently " + unit.value + ", hiding value.")
+                    document.getElementById(unit.id).innerHTML = ""
+                }
             } 
 
             else {
                 document.getElementById(unit.id).innerHTML = ""
             }
-    
         })
     }
 }
-    
-
-
 
 function getDaysInMonths(startDate, numberOfMonths) {
-    // Calculate how many days in a span on months
+    // Calculate how many days in a span of months
     // and return the value in milliseconds
-    console.log("start Get Days In Months (parent)")
     let year = startDate.getFullYear();
     let month = startDate.getMonth();
     let end = month + numberOfMonths
@@ -77,7 +69,6 @@ function getDaysInMonths(startDate, numberOfMonths) {
 
 
 function getDaysInMonth(month, year) {
-    console.log("start Get Days In Month (child)")
     let leapYear = 0;
     var days;
 
@@ -100,7 +91,6 @@ function changeButtonState(id) {
 
     let divID = '#' + id + 'Button'
     let state = timeUnits[id].active;
-    console.log(divID, state);
     
     if (state) {
         $(divID).removeClass('on')
